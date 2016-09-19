b12a8879b319b42a4418d020911079d72f378913
lkroesen <l.kroesen@student.tudelft.nl>
2016-09-19 13:53:24 +0200

    Update app class.

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,9 +1,20 @@
+import java.util.HashMap;
+
 /**
  * Created by Jona on 19-9-2016.
  */
 public class app {
     public static void main(String[] args)
     {
+        for (int i = 0; i < 12; i++)
+            System.out.print(i);
+
+        HashMap<String, String> foo = new HashMap<String, String>();
+        if (foo.isEmpty())
+        {
+            app a = new app();
+            a.returnOne();
+        }
 
     }
 
