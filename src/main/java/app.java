cf811764a3a404de272bcaf79a0f0d00216136ed
Jona Willems <0881451@hr.nl>
2016-09-19 13:55:01 +0200

    Add changes to app class

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -4,6 +4,12 @@
 public class app {
     public static void main(String[] args)
     {
+        int i = 1;
+        int g = 2;
+        int noob = i + g;
+        
+        String x = "noob";
+
 
     }
 
--- src/test/java/appTest.java
+++ src/test/java/appTest.java
@@ -11,4 +11,5 @@ public class appTest {
         assertEquals(a.returnOne(), 1);
     }
 
+
 }
